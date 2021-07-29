20bc7d5483669f3aa4b3e19e1005a55496c72d6e
Darjusch <Darjusch.schrand@code.berlin>
2021-07-29 12:34:36 +0200

    connectedMongoDB

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,26 @@
 const express = require('express')
-
+const UserModel = require('../models/user')
 const registerUser = (req, res, next) => {
-    res.status(200).send("User was successfully registered!");
+    console.log(req.body)
+    const user = UserModel({
+        username: req.body.username,
+        age: req.body.age,
+    })
+    user.save().then(success => {
+        res.status(200).send("User was successfully registered!");
+    }).catch((err) => {
+        res.status(500).send("Something went wrong")
+    })
 }
 exports.registerUser = registerUser;
+
 const getUserProfile = (req, res, next) => {
-    res.status(200).send("John Howard!");
+    UserModel.findOne({ username: req.params.username})
+    .then( user => {
+        res.status(200).send(user);
+    })
+    .catch((err) => {
+        res.status(400).send("User not found!")
+    })
 }
 exports.getUserProfile = getUserProfile;
--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
 const routes = require('./routes/user')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+mongoose
+    .connect('mongodb://localhost:27017/user', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then((res) => {
+        console.log('Database connected!');
+    }
+    )
+    .catch((err) => console.log(err));
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 app.use('/', routes)
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
